c24e7157474faf25533048167a66b7e2149e2000
Bram van den Borne <36791552+vdbBram@users.noreply.github.com>
2023-11-14 11:13:03 +0100

    Change ISCOOL voltage divider factor

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ manual = False # [21.5,26]
 D = Dopplershift_data(mass = 115, scan = scan, voltage_scanning = False, 
 	wn_channel = 'wavenumber_1', wn_bounds = [0,99999999999])
 data = D.extract_raw_data(devices_to_read = D._devices, path = D._PATH)
-data = D.filter_scatter(data = data, filename = 'iscool2', method = 'spline_iscool', ISCOOL_voltage_multiplier =  10000) # apply filter to ISCOOL, for savgol you need to include window_length and polyorder. other options are spline_ISCOOL, avg
+data = D.filter_scatter(data = data, filename = 'iscool2', method = 'spline_iscool', ISCOOL_voltage_multiplier =  10001.645) # apply filter to ISCOOL, for savgol you need to include window_length and polyorder. other options are spline_ISCOOL, avg
 data = D.calibrate_CRIS_voltagemeter(data = data, calibration_factor = 1.) 
 binned_data = D.bin_data(data = data, freq_multiplier = 1) 
 fig,ax = plt.subplots(figsize = (14,9))
